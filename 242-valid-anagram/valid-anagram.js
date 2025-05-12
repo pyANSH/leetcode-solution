724db59bad4c610d963ff065c7acc6fb5637f1da
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2025-05-12 17:53:50 +0530

    Time: 30 ms (23.78%) | Memory: 58.8 MB (35.76%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,28 +4,30 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  if (s.length !== t.length) return false;
+    //   if (s.length !== t.length) return false;
 
-  const createObjWithFrequency = (str) => {
-    let obj = {};
+    //   const createObjWithFrequency = (str) => {
+    //     let obj = {};
 
-    for (let i of str) {
-      if (obj[i]) {
-        obj[i] = obj[i] + 1;
-      } else {
-        obj[i] = 1;
-      }
-    }
-    return obj;
-  };
+    //     for (let i of str) {
+    //       if (obj[i]) {
+    //         obj[i] = obj[i] + 1;
+    //       } else {
+    //         obj[i] = 1;
+    //       }
+    //     }
+    //     return obj;
+    //   };
 
-  const s1 = createObjWithFrequency(s);
-  const s2 = createObjWithFrequency(t);
+    //   const s1 = createObjWithFrequency(s);
+    //   const s2 = createObjWithFrequency(t);
 
-  for (let i in s1) {
-    if (s1?.[i] !== s2?.[i]) {
-      return false;
-    }
-  }
-  return true;
+    //   for (let i in s1) {
+    //     if (s1?.[i] !== s2?.[i]) {
+    //       return false;
+    //     }
+    //   }
+    //   return true;
+    if (s.length !== t.length) return false;
+    return s.split('').sort().join('') === t.split('').sort().join('');
 };
