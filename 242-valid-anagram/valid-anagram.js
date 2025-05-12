b9015cdf9ba843dde63bcd7d5e97c52328e9f04b
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2025-05-12 17:49:26 +0530

    Time: 13 ms (67.27%) | Memory: 57.7 MB (59.57%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  const createObjWithFrequency = (str) => {
+    let obj = {};
+
+    for (let i of str) {
+      if (obj[i]) {
+        obj[i] = obj[i] + 1;
+      } else {
+        obj[i] = 1;
+      }
+    }
+    return obj;
+  };
+
+  const s1 = createObjWithFrequency(s);
+  const s2 = createObjWithFrequency(t);
+
+  for (let i in s1) {
+    if (s1?.[i] !== s2?.[i]) {
+      return false;
+    }
+  }
+  return true;
+};
