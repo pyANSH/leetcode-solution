fd93781b413c85c848f8a95d3086a3ef642118a5
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2025-09-14 20:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 58.1 MB (6.55%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,14 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for (let i=0; i<=nums.length;i++){
-        for (let j=i+1; j <=nums.length;j++){
-            let t = target - nums[i] - nums[j]
-            if (t === 0) {
-                return [i,j]
-            }
-        }
+var twoSum = function (nums, target) {
+    const hash = new Map()
+    for (let i = 0; i <= nums.length; i++) {
+        const need = target - nums[i]
+        if (hash.has(need)) return [hash.get(need), i]
+        hash.set(nums[i], i)
     }
-
+    return 
 };
