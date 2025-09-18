5cb675e71874b0e316ee1a2124ab6d9469634ab8
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2025-09-19 02:24:32 +0530

    Time: 4 ms (86.07%) | Memory: 73 MB (17.65%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -2,11 +2,11 @@ function productExceptSelf(nums) {
     const length = nums.length;
     const arr = new Array(length).fill(1);
     let p = 1;
-    let s = 1;
     for (let i = 0; i < length; i++) {
         arr[i] = p;
         p *= nums[i];
     }
+    let s = 1;
     for (let j = length - 1; j > -1; j--) {
         arr[j] *= s;
         s *= nums[j];
