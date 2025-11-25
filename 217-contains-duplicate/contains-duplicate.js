3d1fb8dbfa4008c3c7e45eb7a91fcc5c63129801
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2025-11-25 07:17:04 +0530

    Time: 36 ms (22.31%) | Memory: 77.4 MB (10.05%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,15 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    if (nums.length === 0) return false
-    const visited = {}
-    for(let i of nums){
-        if(visited[i]) {
-            return true
-        }
-        else {
-            visited[i] = 1
-        }
-    }
-    return false
+  const obj = {}
+
+  for(let i of nums){
+    if(obj[i]) return true
+    obj[i] = 1
+  }
+  return false
+
 };
