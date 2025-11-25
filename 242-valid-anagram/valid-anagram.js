1fda719431ae62adfe36861ef543bbf810f63f34
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2025-11-25 08:06:14 +0530

    Time: 14 ms (58.83%) | Memory: 61 MB (13.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,15 +4,17 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
+    if (s.length !==t.length) return false
+    const CHAR_DEDUCTION = 97
     const arr = new Array(26).fill(0)
     for (let i of s) {
-        let char = i.charCodeAt() - 97
+        let char = i.charCodeAt() - CHAR_DEDUCTION
         arr[char]++
     }
     for (let i of t) {
-        let char = i.charCodeAt() - 97
+        let char = i.charCodeAt() - CHAR_DEDUCTION
         arr[char]--
     }
-    const checkZero = arr.every(i => i === 0)
-    return !!checkZero
+    
+    return !!arr.every(i => i === 0)
 };
