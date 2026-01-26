cfe554005615fcfd90ed26c998caa2ec54549cac
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2026-01-27 01:52:18 +0530

    Time: 4 ms (35.06%) | Memory: 66.4 MB (8.80%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -2,19 +2,12 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (price) {
-    if (price.length === 0) return 0
-    let minPrice = price[0]
-    let maxProfit = 0
-    for (let i = 1; i < price.length; i++) {
-        if (price[i] < minPrice) {
-            minPrice = price[i]
-        }
-        let currentProfit = price[i] - minPrice
-
-        if (currentProfit > maxProfit) {
-            maxProfit = currentProfit
-        }
+var maxProfit = function (prices) {
+    let minPrice = Infinity
+    let profit = 0
+    for (let price of prices) {
+        minPrice = Math.min(minPrice, price)
+        profit = Math.max(profit,  price - minPrice)
     }
-    return maxProfit
+    return profit 
 };
