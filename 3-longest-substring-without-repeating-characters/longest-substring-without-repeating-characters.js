205ac37cab022ea09875e7de366b1e9679931636
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2026-01-31 03:58:28 +0530

    Time: 4 ms (87.77%) | Memory: 56.3 MB (88.51%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -7,15 +7,15 @@ var lengthOfLongestSubstring = function (s) {
     if (len <= 1) return len
     let maxSize = 0
     let i = 0
+    const map = new Map()
     for (let j = 0; j < len; j++) {
-
-        const q = s.indexOf(s[j], i)
-        if (q !== -1 && q < j) {
-            i = q + 1
+        if (map.has(s[j]) && map.get(s[j]) >= i) {
+            i = map.get(s[j]) + 1
         }
+        map.set(s[j], j)
         maxSize = Math.max((j - i) + 1, maxSize)
 
     }
 
-return maxSize
+    return maxSize
 };
