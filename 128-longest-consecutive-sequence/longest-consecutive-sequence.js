227aa9d5b85eb8646b649407ad3e9b275feee4e5
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2026-05-18 07:59:37 +0530

    Time: 32 ms (94.97%) | Memory: 80.6 MB (57.74%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -3,20 +3,20 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
-    let unique = new Set(nums);
-    let highest = 0
-    for (let val of unique) {
-        if (!unique.has(val - 1)) {
-            let cur = val;
-            let len = 1;
-            while (unique.has(cur + 1)) {
-                cur++;
-                len++;
-            }
-            if (len > highest) highest = len;
+    const unique = new Set(nums);
+    let best = 0
+    for (let n of unique) {
+        if (unique.has(n - 1)) continue
+        let curr = n
+        let len = 1
+
+        while (unique.has(curr + 1)) {
+            curr++
+            len++
         }
+        best = Math.max(best, len)
     }
-    return highest;
+    return best;
 }
 
 
